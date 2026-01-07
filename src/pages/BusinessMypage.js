97d1744819c1074d034e2bc625f5cd1e94045a2b
dahye0937 <dahye0937@gmail.com>
2026-01-07 14:36:27 +0900

    update

--- src/pages/BusinessMypage.js
+++ src/pages/BusinessMypage.js
@@ -1,9 +1,59 @@
 // --- 사업자 마이페이지 ---
 
+import { useNavigate } from "react-router-dom";
+import "./BusinessMypage.scss";
+import Staylogo from "../assets/images/Logo/EGWH_logo.png";
+
 const BusinessMypage = () => {
+  const navigate = useNavigate();
+  const mypageData = {
+    role: "사업자",
+    building: "0000",
+    nuit: "0000",
+    name: "온담 감자탕",
+    carNumber: "12가3456",
+    parkingLocation: "A1-6번",
+  };
+
   return (
-    <div>
-      <h1>사업자</h1>
+    <div className="business-mypage">
+      {/* 상단 카드 */}
+      <section className="business-card">
+        <img src={Staylogo} alt="EGWH 로고" className="stay-logo" />
+        <div className="mypage-info">
+          <span className="role">{mypageData.role}</span>
+          <p className="address">
+            {mypageData.building}동 {mypageData.nuit} 호
+          </p>
+          {/* 사업자만 상호명 노출 */}
+          {mypageData.name && (
+            <h2 className="name">{mypageData.name}</h2>
+          )}
+          <span className="parking">
+            현재 주차 위치: {mypageData.parkingLocation}
+          </span>
+        </div>
+        {/* 카드 버튼 */}
+        <div className="mypage-actions">
+          <button>차량 정보 수정</button>
+          <button>문의 하기</button>
+          <button>이용 가이드</button>
+        </div>
+      </section>
+      {/* 메뉴 리스트 */}
+      <section className="mypage-menu">
+        <button>공지사항</button>
+        <button
+        onClick={()=>{
+          navigate("/app/business/visited");
+        }}
+        >
+          방문했던 차량
+        </button>
+        <button>자주 묻는 질문</button>
+      </section>
+      {/* 로그아웃 */}
+      <button className="mypage-logout">로그아웃</button>
     </div>
   );
 };
--- src/pages/ResidentMypage.js
+++ src/pages/ResidentMypage.js
@@ -1,13 +1,52 @@
 // --- 입주민 마이페이지 ---
 
 import { useNavigate } from "react-router-dom";
+import EGWHLogo from "../assets/images/Logo/EGWH_logo.png";
+import "./ResidentMypage.scss";
 
 const ResidentMypage = () => {
   const navigate = useNavigate();
+  //DB 연동 예정(더미 데이터)
+  const mypageData = {
+    role: "입주민",
+    building: "0000",
+    nuit: "0000",
+    carNumber: "12가3456",
+    parkingLocation: "B3-2번",
+  };
 
   return (
-    <div>
-      <h1>입주자</h1>
+    <div className="resident-mypage">
+      {/* 상단 카드 */}
+      <section className="resident-card">
+        <img  src= {EGWHLogo} alt="EGWH 로고" className="brand-logo"/>
+        <div className="mypage-info">
+          <span className="role">{mypageData.role}</span>
+          <p className="address">
+            {mypageData.building}동 {mypageData.nuit}호
+          </p>
+          <h2 className="car-number">{mypageData.carNumber}</h2>
+          <span className="parking">
+            현재 주차 위치: {mypageData.parkingLocation}
+          </span>
+      </div>
+      {/* 카드 버튼 */}
+      <div className="card-actions">
+        <button>차량 정보 수정</button>
+        <button>문의 하기</button>
+        <button>이용 가이드</button>
+      </div>
+      </section>
+      {/* 메뉴 리스트 */}
+      <section className="menu-list">
+        <button>공지사항</button>
+        <button
+          onClick={()=>{
+            navigate("/app/resident/visited");
+          }}
+        >
+          방문했던 차량
+        </button>
       <button
         onClick={() => {
           navigate("/app/resident/favorite");
@@ -15,6 +54,10 @@ const ResidentMypage = () => {
       >
         즐겨 찾는 차량
       </button>
+      <button>자주 묻는 질문</button>
+      </section>
+      {/* 로그아웃 */}
+      <button className="logout-btn">로그아웃</button>
     </div>
   );
 };
